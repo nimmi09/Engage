52f36e221d61f0a37c43a94b09f93e2653735bf8
Namrata <namratapant2002@gmail.com>
2022-05-11 21:23:59 +0530

    face detection

---
+++ face.py
@@ -0,0 +1,47 @@
+import sys
+import json
+import cv2
+import numpy as np
+import face_recognition
+import os
+
+rows=json.loads(sys.argv[2])
+images=[]
+criminal_id=[]
+print(rows[0]['path'])
+for row in rows:
+    curImg = cv2.imread(row['path'])
+    images.append(curImg)
+    criminal_id.append(row['offender_id'])
+    
+imgTest = face_recognition.load_image_file(sys.argv[1])
+imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
+faceLocTest = face_recognition.face_locations(imgTest)[0]
+encodeTest = face_recognition.face_encodings(imgTest)[0]
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+        
+    return encodeList
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+ 
+
+
+
+
+ 
+matches = face_recognition.compare_faces(encodeListKnown,encodeTest)
+faceDis = face_recognition.face_distance(encodeListKnown,encodeTest)
+print(matches,faceDis)
+
+matchIndex = np.argmin(faceDis)
+if matches[matchIndex]:
+    print(criminal_id[matchIndex])
+print(matches,faceDis)
+
+
+ 
