3683917c5fb3592af62e306944bf023a5b4233ab
Namrata <namratapant2002@gmail.com>
2022-05-17 11:45:56 +0530

    offender class created

---
+++ offender.js
@@ -0,0 +1,11 @@
+class offender{
+    constructor(user_id,age,gender,date_added,name) {
+    this.user_id=user_id;
+    this.age=age;
+    this.gender=gender;
+    this.date_added=date_added;
+    this.name=name;
+   
+    }
+}
+module.exports={offender};
