db0d144c97c96234bff138f5f5c3658d7abb2cb9
Namrata <namratapant2002@gmail.com>
2022-05-27 13:48:14 +0530

    fixing paths

--- file-utility.js
+++ file-utility.js
@@ -1,20 +1,26 @@
 const fs = require("fs");
+const path = require("path");
 class file_utility {
+
   static movefile(image_path) {
-    let pathparts = image_path.split("\\");
+    console.log(image_path, 'before replace')
+    var new_image_path = image_path.replace(/\\/g,'/')
+    console.log(abc,'2 replace')
+    let pathparts = new_image_path.split("/");
+    console.log(pathparts,'23');
     let filename = pathparts[pathparts.length - 1];
-    console.log(filename);
+    console.log(filename,'45');
     const destinationPath =
       "./uploads/permanent" +
       "/" +
       filename;
-      console.log(destinationPath)
+      console.log(destinationPath,'56')
     fs.rename(image_path, destinationPath, function (err) {
       if (err) {
         throw err;
       }
     });
-    console.log(destinationPath);
+    
     return destinationPath;
   }
   static deletefile(images) {
